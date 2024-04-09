6bbc63ecc7fde17dbd5763303fa1b0a654a67238
Lara Lindsay <laralindsay00@outlook.com>
2024-04-09 17:33:49 -0300

    add menu button

--- src/app/containers/main/main.module.ts
+++ src/app/containers/main/main.module.ts
@@ -2,11 +2,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/components/header/header.component';
 import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main-routing.module';
-import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   imports: [CommonModule, MainRoutingModule, HeaderComponent],
-  declarations: [MainComponent,  NavbarComponent],
+  declarations: [MainComponent],
 })
 export class MainModule {}
